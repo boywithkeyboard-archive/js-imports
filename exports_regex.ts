db8a5e9cbe085903de5a8cf1b368a1bdd52a2441
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2024-04-03 18:33:41 +0200

    refactor: edit regex group name

--- exports_regex.ts
+++ exports_regex.ts
@@ -1,2 +1,2 @@
 export const exportsRegex =
-  /export\s+((\*(\s+as\s+[a-zA-Z$_][0-9a-zA-Z$_]*)?)|((type\s+)?{((\s+)?(type\s+)?[a-zA-Z$_][0-9a-zA-Z$_]*(\s+as\s+[a-zA-Z$_][0-9a-zA-Z$_]*)?(\s)?)((\s+)?,(\s+)?(type\s+)?[a-zA-Z$_][0-9a-zA-Z$_]*(\s+as\s+[a-zA-Z$_][0-9a-zA-Z$_]*)?(\s)?)*}))\s+from\s+('|")(?<specifier>[^"']+)('|")(;|\n|\r\n)/gm
+  /export\s+((\*(\s+as\s+[a-zA-Z$_][0-9a-zA-Z$_]*)?)|((type\s+)?{((\s+)?(type\s+)?[a-zA-Z$_][0-9a-zA-Z$_]*(\s+as\s+[a-zA-Z$_][0-9a-zA-Z$_]*)?(\s)?)((\s+)?,(\s+)?(type\s+)?[a-zA-Z$_][0-9a-zA-Z$_]*(\s+as\s+[a-zA-Z$_][0-9a-zA-Z$_]*)?(\s)?)*}))\s+from\s+('|")(?<identifier>[^"']+)('|")(;|\n|\r\n)/gm
--- imports_regex.ts
+++ imports_regex.ts
@@ -1,2 +1,2 @@
 export const importsRegex =
-  /import(\s+(type\s)?([a-zA-Z$_][0-9a-zA-Z$_]*(\s)?,(\s)?)?((((\*\s+as)\s+)?[a-zA-Z$_][0-9a-zA-Z$_]*)|({((\s+)?(type\s+)?[a-zA-Z$_][0-9a-zA-Z$_]*(\s+as\s+[a-zA-Z$_][0-9a-zA-Z$_]*)?(\s)?)((\s+)?,(\s+)?(type\s+)?[a-zA-Z$_][0-9a-zA-Z$_]*(\s+as\s+[a-zA-Z$_][0-9a-zA-Z$_]*)?(\s)?)*}))\s+from)?\s+('|")(?<specifier>[^"']+)('|")(;|\n|\r\n)/gm
+  /import(\s+(type\s)?([a-zA-Z$_][0-9a-zA-Z$_]*(\s)?,(\s)?)?((((\*\s+as)\s+)?[a-zA-Z$_][0-9a-zA-Z$_]*)|({((\s+)?(type\s+)?[a-zA-Z$_][0-9a-zA-Z$_]*(\s+as\s+[a-zA-Z$_][0-9a-zA-Z$_]*)?(\s)?)((\s+)?,(\s+)?(type\s+)?[a-zA-Z$_][0-9a-zA-Z$_]*(\s+as\s+[a-zA-Z$_][0-9a-zA-Z$_]*)?(\s)?)*}))\s+from)?\s+('|")(?<identifier>[^"']+)('|")(;|\n|\r\n)/gm
