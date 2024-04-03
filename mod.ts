f8d1280c82351048326ee183f6743f0ee6861624
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2024-04-03 18:03:16 +0200

    feat: create entry file

---
+++ mod.ts
@@ -0,0 +1,4 @@
+export { denoTypesCommentRegex } from './deno_types_comment_regex.ts'
+export { exportsRegex } from './exports_regex.ts'
+export { importsRegex } from './imports_regex.ts'
+export { tripleSlashCommentRegex } from './triple_slash_comment_regex.ts'
